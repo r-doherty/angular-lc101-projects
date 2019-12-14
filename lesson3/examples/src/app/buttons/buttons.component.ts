64eb62c5413e0371a3d0b5d44fdf5332e71f4c3f
Ryan Doherty <rjd.atx@gmail.com>
2019-12-13 23:27:27 -0600

    30. Angular, Part 3 Readings + Examples

--- lesson3/examples/src/app/buttons/buttons.component.ts
+++ lesson3/examples/src/app/buttons/buttons.component.ts
@@ -7,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ButtonsComponent implements OnInit {
    buttonHeading: string = "Buttons"
-   inactive: boolean = false;
+   activationArray: boolean[] = [false, false, false]
+   active: boolean = true;
+   location: string = 'center';
 
    constructor() { }
 
    ngOnInit() { }
+
+   resetActivations()
+   {
+      for(let i = 0; i < this.activationArray.length; i++)
+      {
+         if(this.activationArray[i] === true)
+         {
+            this.activationArray[i] = false;
+         }
+      }
+   }
+
+   shiftLocation(oldLocation: string) {
+      while (this.location === oldLocation) {
+         this.location = ['left', 'right', 'center'][Math.floor(Math.random()*3)];
+      }
+      return this.location;
+   }
 
 }
